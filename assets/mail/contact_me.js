012aca9f6ae808b961342a7ddd20f4a8d652ca7b
Emmanuel Bidemi Grace <115803464+G-gracie@users.noreply.github.com>
2024-05-23 15:28:46 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: https://f5q1q1u1ed.execute-api.eu-west-1.amazonaws.com/default/G-cloud-predict",
+                url: https://p1r8owbcl8.execute-api.eu-west-1.amazonaws.com",
                  
                 type: "POST",
                 data: JSON.stringify(json),
